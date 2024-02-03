cb07f4bae868b3f315886f11f899a60ce73ac99e
Michele Appiani <michele.appiani@gmail.com>
2024-02-03 10:58:26 +0100

    Copy data condition and animation pages

---
+++ samples/XAMLBehaviorsSample/XAMLBehaviorsSample.Net8/DataPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XAMLBehaviorsSample.Net8
+{
+    public partial class DataPage : UserControl
+    {
+        private DataStateControl _dataStateControl;
+        private SetDataValueControl _setDataValueControl;
+
+        public DataPage()
+        {
+            this.InitializeComponent();
+            this._dataStateControl = new DataStateControl();
+            this._setDataValueControl = new SetDataValueControl();
+        }
+
+        private void DataStateBehavior_Click(object sender, RoutedEventArgs e)
+        {
+            this.MainContent.Children.Clear();
+            this.MainContent.Children.Add(this._dataStateControl);
+        }
+
+        private void SetDataStoreValueAction_Click(object sender, RoutedEventArgs e)
+        {
+            this.MainContent.Children.Clear();
+            this.MainContent.Children.Add(this._setDataValueControl);
+        }
+    }
+}
--- samples/XAMLBehaviorsSample/XAMLBehaviorsSample.Net8/MainWindow.xaml.cs
+++ samples/XAMLBehaviorsSample/XAMLBehaviorsSample.Net8/MainWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace XAMLBehaviorsSample.Net8;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     private AnimationPage animationPage;
