0abb2bd905e7f98ae65fc8d2be92e1df5cf280af
Michele Appiani <michele.appiani@gmail.com>
2024-02-03 11:10:44 +0100

    animation section builds

---
+++ samples/XAMLBehaviorsSample/XAMLBehaviorsSample.Net8/Animation/MouseDragElementControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace XAMLBehaviorsSample.Net8.Animation
+{
+    public partial class MouseDragElementControl : UserControl
+    {
+        public MouseDragElementControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/XAMLBehaviorsSample/XAMLBehaviorsSample.Net8/Animation/TouchBehaviorControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XAMLBehaviorsSample.Net8.Animation
+{
+    /// <summary>
+    /// Interaction logic for TouchBehaviorControl.xaml
+    /// </summary>
+    public partial class TouchBehaviorControl : UserControl
+    {
+        public TouchBehaviorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
